dddd8b94ec4378699307efa6e5b57d36137bc527
Adityasharda <adityaprksraj@gmail.com>
2017-02-16 13:09:52 +0530

    Update Main2.java

--- Main2.java
+++ Main2.java
@@ -1,3 +1,6 @@
+/*This module for the checking of simple encryption and decryption of the string using AES.
+simple image encryption and decryption will be done after this module.
+*/
 package com.encrypt;
 
 import java.security.InvalidKeyException;
@@ -17,20 +20,23 @@ public class Main2 {
 
 	public static void main(String[] args) throws Exception 
 	{
+		/* this part generates the 128 bit key using AES*/
 		KeyGenerator keygen=KeyGenerator.getInstance("AES");
 		keygen.init(128);
 		SecretKey secretkey=keygen.generateKey();
 		cipher=Cipher.getInstance("AES");
 		
 		String plaintxt="hello how r u?";
-		String key=secretkey.toString();
+		String key=secretkey.toString();//Here a key is generated.
 		System.out.println(" secret key: "+key);
-		String encryptedtxt=encrypt(plaintxt, secretkey);
+		String encryptedtxt=encrypt(plaintxt, secretkey);//This module encrypt the plaintext using key.
 		System.out.println("encrypted txt: "+encryptedtxt);
 		
-		String decryptedtxt=decrypt(encryptedtxt, secretkey);
+		String decryptedtxt=decrypt(encryptedtxt, secretkey);//This module decrypt the encrypted text and return original value.
 		System.out.println("decrypted txt: "+decryptedtxt);
 	}
+	
+	/* This module for the encryption of the text*/
 	public static String encrypt(String plaintxt,SecretKey secretkey) throws Exception
 	{
 		byte [] plaintxtbyte=plaintxt.getBytes();
@@ -43,6 +49,8 @@ public class Main2 {
 		return encryptedtxt;
 		
 	}
+	
+	/* This module for the decription of the encrypted text*/
 	public static String decrypt(String encryptedtxt,SecretKey secretkey) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException
 	{
 		Base64.Decoder decoder=Base64.getDecoder();
